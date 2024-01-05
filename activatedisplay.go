0781c4df3829becc2d59c35c055fd9679fb2f11f
Zaheer Abbas Merali <zaheermerali@gmail.com>
2024-01-05 22:58:38 +0000

    Switch to using mqtt rather than require direct http access

--- activatedisplay.go
+++ activatedisplay.go
@@ -1,24 +1,75 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	"fmt"
+	"io/ioutil"
 	"os/exec"
+
+	MQTT "github.com/eclipse/paho.mqtt.golang"
+	"gopkg.in/yaml.v2"
 )
 
-func main() {
-	http.HandleFunc("/activate-display", activateDisplay)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+type Config struct {
+	MQTT struct {
+		Server   string `yaml:"server"`
+		Topic    string `yaml:"topic"`
+		Username string `yaml:"username"`
+		Password string `yaml:"password"`
+	} `yaml:"mqtt"`
 }
 
-func activateDisplay(w http.ResponseWriter, r *http.Request) {
-	cmd := exec.Command("xset", "dpms", "force", "on")
-	cmd.Env = append(os.Environ(), "DISPLAY=:0")
-	err := cmd.Run()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+var messagePubHandler MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
+	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
+	if string(msg.Payload()) == "activate" {
+		cmd := exec.Command("xset", "dpms", "force", "on")
+		err := cmd.Run()
+		if err != nil {
+			fmt.Println("Error executing command:", err)
+		} else {
+			fmt.Println("Display activated")
+		}
 	}
-	w.Write([]byte("Display activated"))
+}
+
+var connectHandler MQTT.OnConnectHandler = func(client MQTT.Client) {
+	fmt.Println("Connected")
+}
+
+var connectLostHandler MQTT.ConnectionLostHandler = func(client MQTT.Client, err error) {
+	fmt.Printf("Connect lost: %v", err)
+}
+
+func main() {
+	// Reading the YAML configuration file
+	configFile, err := ioutil.ReadFile("config.yaml")
+	if err != nil {
+		panic(err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(configFile, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Setting up MQTT client options
+	opts := MQTT.NewClientOptions()
+	opts.AddBroker(config.MQTT.Server)
+	opts.SetClientID("go_mqtt_client")
+	opts.SetUsername(config.MQTT.Username)
+	opts.SetPassword(config.MQTT.Password)
+	opts.SetDefaultPublishHandler(messagePubHandler)
+	opts.OnConnect = connectHandler
+	opts.OnConnectionLost = connectLostHandler
+
+	client := MQTT.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	token := client.Subscribe(config.MQTT.Topic, 1, nil)
+	token.Wait()
+	fmt.Printf("Subscribed to topic: %s\n", config.MQTT.Topic)
+
+	select {}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
 module activatedisplay
 
 go 1.21.4
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.4.3 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
